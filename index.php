46cc4b6bfa33667e4695cf1e1bcc8203317c8aa9
wyang <wangyang@boyuntong.com>
2017-03-09 14:27:07 +0800

    'dupaere'

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
-<?php 
-
-echo time();
-?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>莫慌，抱紧哥</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
 	<script src="./jquery-2.1.1.js"></script>
 </head>
 <body>
-	<h3>来呀，互相伤害啊！！！</h3>
-	<p>酷酷哒</p>
-	<p><img src="https://www.baidu.com/img/38270_b8c2a909848579fb12dc61f16ea9cde2.gif"></p>
+	<div id="content">
+		<h3>Time: <?= date() ?></h3>
+		<h3>来呀，互相伤害啊！！！</h3>
+		<p class="w-50"><img src="./img.jpg" title="莫慌，抱紧哥"></p>
+	</div>
 </body>
 </html>
 
